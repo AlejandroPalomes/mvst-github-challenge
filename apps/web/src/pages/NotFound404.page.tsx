d897580d439359107338f5f675e838d67f8123b0
Alejandro Palomes <alejandropalomes@hotmail.com>
2023-12-05 17:09:45 +0100

    test useRouter in 404 page

--- apps/web/src/pages/NotFound404.page.tsx
+++ apps/web/src/pages/NotFound404.page.tsx
@@ -1,7 +1,16 @@
 import { FC } from 'react';
+import { useRouter } from '../hooks/router/useRouter.tsx';
+import { Public } from '../router/routes/Public.ts';
 
 export const NotFound404Page: FC = () => {
-  return <>
-    404 not found...
-  </>;
+  const { goBack } = useRouter();
+
+  return (
+    <div>
+      <p>
+        404 not found...
+      </p>
+      <button onClick={() => goBack(Public.HOME.route)}>Go back</button>
+    </div>
+  );
 };
