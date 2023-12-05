09e400bcb11d80ee8e30907f0dbbdd5fd002c31a
Alejandro Palomes <alejandropalomes@hotmail.com>
2023-12-05 01:58:24 +0100

    first axios with github graphQL attempt

--- apps/web/src/hooks/api/useGet.tsx
+++ apps/web/src/hooks/api/useGet.tsx
@@ -2,9 +2,9 @@
 // import { isAClientError, isUnauthorizedError } from '../../lib/network/helpers';
 import axios from 'axios';
 
-const MAX_RETRY = 3;
+// const MAX_RETRY = 3;
 
-const GTIHUB_BASE_URL = 'https://api.github.com/graphql';
+// const GTIHUB_BASE_URL = 'https://api.github.com/graphql';
 
 export type Fetcher<T> = () => Promise<T>;
 
@@ -24,7 +24,44 @@ interface TempGithubResponse {}
 // export const useGet = <T extends any>(uniqueKey: QueryKey, fetcher: Fetcher<T>, options?: UseGetOptions): UseAPIResult<T> => {
 export const useGet = <T extends TempGithubResponse>(): UseAPIResult<T> => {
 
-  const response = axios;
+  //TODO Move this to a file named API > API.users.find, and both variables need to be params from that
+  const username = 'AlejandroPalomes';
+  const partialString = 'mvst'
+  const query = `
+{
+  search(query: "user:${username} ${partialString}", type: REPOSITORY, first: 5) {
+    edges {
+      node {
+        ... on Repository {
+          name
+          owner {
+            login
+          }
+          description
+          createdAt
+        }
+      }
+    }
+  }
+}
+`;
+
+let data;
+  axios.post('https://api.github.com/graphql', {
+    query: query
+  }, {
+    headers: {
+      // Authorization: `Bearer ${token}`,
+    },
+  })
+    .then(response => {
+      const repositories = response.data.data.search.edges.map((edge: any) => edge.node); //TODO Remove any type
+      data = repositories;
+      console.log(repositories);
+    })
+    .catch(error => {
+      console.error('Error:', error.response ? error.response.data : error.message);
+    });
 
   // const { isLoading, error, isError, data, refetch, status } = useQuery<T>(uniqueKey, fetcher, {
   //   notifyOnChangeProps: ['data', 'error'],
@@ -64,7 +101,7 @@ export const useGet = <T extends TempGithubResponse>(): UseAPIResult<T> => {
   // }
 
   return {
-    data: response
+    data
     // isLoading,
     // error,
     // refetch,
