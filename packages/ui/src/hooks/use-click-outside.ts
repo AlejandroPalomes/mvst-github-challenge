fca390fa139825d1082f99255911a1e68c26ccb2
Alejandro Palomes <alejandropalomes@hotmail.com>
2023-12-07 22:10:05 +0100

    document useClickOutside hook

--- packages/ui/src/hooks/use-click-outside.ts
+++ packages/ui/src/hooks/use-click-outside.ts
@@ -4,6 +4,12 @@ import type React from 'react';
 type RefType<T> = React.RefObject<T>;
 type HandlerType = (event?: MouseEvent | TouchEvent) => void;
 
+/**
+ * Custom hook used to detect a click outside the referred elemnt and trigger an action
+ * 
+ * @param ref - The reference for the element to be observed.
+ * @param handler - Callback function to execute once a click is made outside the reference elemnt.
+ */
 export const useClickOutside = <T extends HTMLElement>(ref: RefType<T>, handler: HandlerType) => {
   useEffect(() => {
     const listener = (event: MouseEvent | TouchEvent) => {
