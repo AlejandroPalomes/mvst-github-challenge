88ba758f8db1f7df44efe391638f44be9ad738be
Alejandro Palomes <alejandropalomes@hotmail.com>
2023-12-08 02:18:49 +0100

    add useDebounce hook

---
+++ apps/web/src/hooks/helpers/useDebounce.ts
@@ -0,0 +1,34 @@
+import { useEffect, useRef } from 'react';
+
+/**
+ * Debouncer function to perform an action with certain delay
+ * 
+ * @param callback - Callback is invoked after the specified delay
+ * @param delay - Specifies the debounce delay in milliseconds
+ * @returns Funcrion that checks if there is an existing timeout and clears it
+ *          before setting a new timeout using setTimeout
+ */
+export const useDebounce = (callback: (...args: any) => void, delay: number = 500) => {
+  const timeoutRef = useRef<any>(null);
+
+  useEffect(() => {
+    // Cleanup the previous timeout on re-render
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, []);
+
+  const debouncedCallback = (...args: any[]) => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+
+    timeoutRef.current = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+
+  return debouncedCallback;
+};
